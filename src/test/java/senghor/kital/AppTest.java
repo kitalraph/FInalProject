2a5c5c3905a2ed3f23acec578313562421514d16
gorgui kital <gorguikitalgithub@gmail.com>
2020-11-26 18:21:03 +0000

    my second commit

--- src/test/java/senghor/kital/AppTest.java
+++ src/test/java/senghor/kital/AppTest.java
@@ -1,7 +1,9 @@
 package senghor.kital;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
+
+import calcule.Calculator;
 import org.junit.Test;
 
 /**
@@ -9,12 +11,29 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    Calculator calculator=new Calculator();
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void sumTest(){
+        int result = calculator.sum(2,4);
+        assertEquals(6,result);
     }
+
+    @Test
+    public void minusTest(){
+        int result = calculator.minus(2,4);
+        assertEquals(-2,result);
+    }
+
+    @Test
+    public void divideTest(){
+        int result = calculator.divide(2,4);
+        assertEquals(0,result);
+    }
+
+    @Test
+    public void MultiplyTest(){
+        int result = calculator.multiply(2,4);
+        assertEquals(8,result);
+    }
+
 }
